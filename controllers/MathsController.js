8c777997c12c9d3da2aed17d63ab189b9457a833
ArianneV11 <76137873+ArianneV11@users.noreply.github.com>
2022-09-13 09:13:34 -0400

    change in controller/html

--- controllers/MathsController.js
+++ controllers/MathsController.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const fs = require('fs');
+const { threadId } = require('worker_threads');
 module.exports =
     class MathsController extends require('./Controller') {
         constructor(HttpContext) {
@@ -14,17 +15,75 @@ module.exports =
             }
             else{
                 if(this.HttpContext.path.params.op){
-                    if(this.HttpContext.path.params.op == ' ')
-                        this.HttpContext.path.params.op = '+';
                     switch(this.HttpContext.path.params.op){
-                        case '+' : break;
-                    }
-
+                        case " " || "+":
+                            this.HttpContext.path.params.value = parseInt(this.HttpContext.path.params.x) + parseInt(this.HttpContext.path.params.y);
+                            if(this.HttpContext.path.params.op == ' ')
+                                this.HttpContext.path.params.op = '+';
+                            //else{
+                                //let respondObj = {error: "op is missing"};
+                                this.HttpContext.response.JSON(this.HttpContext.path.params);
+                           // }
+                            break;    
+                        case "-":
+                            this.HttpContext.path.params.value = parseInt(this.HttpContext.path.params.x) - parseInt(this.HttpContext.path.params.y);
+                            this.HttpContext.response.JSON(this.HttpContext.path.params);
+                            break;
+                        case "*":
+                            this.HttpContext.path.params.value = parseInt(this.HttpContext.path.params.x) * parseInt(this.HttpContext.path.params.y);
+                            this.HttpContext.response.JSON(this.HttpContext.path.params);
+                            break;
+                        case "/":
+                            this.HttpContext.path.params.value = parseInt(this.HttpContext.path.params.x) / parseInt(this.HttpContext.path.params.y);
+                            this.HttpContext.response.JSON(this.HttpContext.path.params);
+                            break;
+                        case "%":
+                            this.HttpContext.path.params.value = parseInt(this.HttpContext.path.params.x) % parseInt(this.HttpContext.path.params.y);
+                            this.HttpContext.response.JSON(this.HttpContext.path.params);
+                            break;
+                        case "!":
+                            this.HttpContext.path.params.value = factorial(parseInt(this.HttpContext.path.params.n));
+                            this.HttpContext.response.JSON(this.HttpContext.path.params);
+                            break;
+                        case "p":
+                            this.HttpContext.path.params.value = isPrime(parseInt(this.HttpContext.path.params.n));
+                            this.HttpContext.response.JSON(this.HttpContext.path.params);
+                            break;
+                        case "np":
+                            this.HttpContext.path.params.value = findPrime(parseInt(this.HttpContext.path.params.n));
+                            this.HttpContext.response.JSON(this.HttpContext.path.params);
+                            break;
+                        }
                 }else{
-                    let respondObj = {error: "op is missing"};
-                    this.HttpContext.response.JSON(respondObj);
+
                 }
             }
                 
         }
+    }
+    function factorial(n) {
+        if (n === 0 || n === 1) {
+            return 1;
+        }
+        return n * factorial(n - 1);
+    }
+
+    function isPrime(value) {
+        for (var i = 2; i < value; i++) {
+            if (value % i === 0) {
+                return false;
+            }
+        }
+        return value > 1;
+    }
+
+    function findPrime(n) {
+        let primeNumer = 0;
+        for (let i = 0; i < n; i++) {
+            primeNumer++;
+            while (!isPrime(primeNumer)) {
+                primeNumer++;
+            }
+        }
+        return primeNumer;
     }
